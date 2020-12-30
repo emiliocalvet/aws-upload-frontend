c7de6fe129cb517d83c35312a5921e27a798dd22
Emilio de Aquino Calvet <emiliocalvet@gmail.com>
2020-12-30 14:42:18 -0300

    front finished

---
+++ src/components/Upload/index.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react'
+import Dropzone from 'react-dropzone'
+
+import { DropContainer, UploadMessage } from './styles'
+
+export default class Upload extends Component {
+  renderDragMessage = (isDragActive, isDragReject) => {
+    if (!isDragActive) {
+      return <UploadMessage>Drag the files here</UploadMessage>
+    }
+
+    if (isDragReject) {
+      return <UploadMessage type="error">File not supported</UploadMessage>
+    }
+
+    if (isDragActive) {
+      return <UploadMessage type="success">Drop the files here</UploadMessage>
+    }
+  }
+
+  render() {
+    const { onUpload } = this.props
+
+    return (
+      <Dropzone accept="image/*" onDropAccepted={onUpload}>
+        {({ getRootProps, getInputProps, isDragActive, isDragReject }) => (
+          <DropContainer
+            {...getRootProps()}
+            isDragActive={isDragActive}
+            isDragReject={isDragReject}
+          >
+            <input {...getInputProps()} />
+            {this.renderDragMessage(isDragActive, isDragReject)}
+          </DropContainer>
+        )}
+      </Dropzone>
+    )
+  }
+}
